4954e38d12b39cb7bbdad8c370a78fae91fd84b7
perfecttosh <115477777+perfecttosh@users.noreply.github.com>
2023-11-10 00:52:12 +0300

    Вариативное домашнее задание за 11.11.2023
    Функции if else else if

---
+++ Дз 11.11.2023/Program.cs
@@ -0,0 +1,18 @@
+﻿//Перов №16 с.15
+double x = Convert.ToInt32(Console.ReadLine());
+double y = Convert.ToInt32(Console.ReadLine());
+int R = 6;
+double S = x * x + y * y;
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("False");
+}
+else if(S < R * R)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
